3f24dd4ebb5b0dfabc670ec245ebc5270fab759d
CATANYASH <71903281+CATANYASH@users.noreply.github.com>
2023-12-04 18:18:53 +0700

    Add Reddit Spawner

--- Assets/Scripts/Features/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Features/Enemies/EnemySpawner.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace SibJam.Features.Enemies
 {
@@ -12,7 +13,19 @@ namespace SibJam.Features.Enemies
         private EnemySpawnerConfig _config;
 
         [SerializeField]
-        private Vector2[] _redditPoints;  
+        private Transform[] _redditPoints;
+
+        public Vector2 TopRight;
+        public Vector2 TopLeft;
+        public Vector2 BottomRight;
+        public Vector2 BottomLeft;
+
+
+
+        private Queue<EnemyBase> _curEnemies = new();
+        private int _curLevel = 1;
+
+        private const int MAX_LVL = 3;
 
 
         // Start is called before the first frame update
@@ -23,7 +36,7 @@ namespace SibJam.Features.Enemies
 
         private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.L)) 
+            if (Input.GetKeyDown(KeyCode.L) && _curLevel < MAX_LVL) 
             {
                 OnLevelUp();
             }
@@ -31,17 +44,43 @@ namespace SibJam.Features.Enemies
 
         private void OnLevelUp()
         {
+            DestroyEnemies();
 
+            _curLevel++;
+
+            Spawn();
         }
 
         private void Spawn()
         {
-            throw new NotImplementedException();
+            SpawnReddit();
         }
 
         private void SpawnReddit() 
         {
+            for (int i = 0;  i < _config.RedditPerLevelCount[_curLevel-1]; i++) 
+            {
+                //Vector2 rnd = new Vector2(Random.Range(BottomLeft.x, BottomRight.x),
+                //   Random.Range(BottomRight.y, TopRight.y));
 
+
+
+                Vector2 pos = _redditPoints[i].position; 
+
+                EnemyBase enemy = 
+                    Instantiate (_config.EnemyReddit, pos, Quaternion.identity);
+
+                _curEnemies.Enqueue(enemy);
+
+            }
+        }
+        private void DestroyEnemies() 
+        {
+            while (_curEnemies.Count > 0)
+            { 
+                EnemyBase obj = _curEnemies.Dequeue();
+                Destroy(obj.gameObject);
+            }
         }
 
        
